42419595ca2fb79a14ca1011a6027f20d69e81b0
Saher Pathan <sahergpathan@gmail.com>
2024-04-08 07:26:11 +0530

    Added files

--- Protein_coding/ROC_Analysis/NC_ROC.py
+++ Protein_coding/ROC_Analysis/NC_ROC.py
@@ -1,19 +1,20 @@
 import pandas as pd
 import matplotlib
-matplotlib.use('Agg')
-
-
+matplotlib.use('Agg')  # Using the Agg backend for non-interactive plotting
 import matplotlib.pyplot as plt
 from sklearn.metrics import roc_auc_score, roc_curve
 
 # Read the data from CSV
-data = pd.read_csv("datasets/Diff_genes_heatmap_NSIP-HEALTHY.csv")
+data = pd.read_csv("../../datasets/Diff_genes_heatmap_NSIP-CHP.csv")
 
 # Filter columns starting with "NSIP" and "HEALTHY"
-columns_to_use = [col for col in data.columns if col.startswith("NSIP") or col.startswith("HEALTHY")]
+columns_to_use = [col for col in data.columns if col.startswith("NSIP") or col.startswith("CHP")]
 
 # Calculate the mean significance value for each gene
 data['mean_significance'] = data[columns_to_use].mean(axis=1)
+
+# Filter genes with Type as "protein_coding"
+data = data[data['Type'] == 'protein_coding']
 
 # Assign labels based on the 'Regulation' column
 labels = (data['Regulation'] == 'Up').astype(int)
@@ -21,6 +22,7 @@ labels = (data['Regulation'] == 'Up').astype(int)
 # Calculate ROC AUC
 auc = roc_auc_score(labels, data['mean_significance'])
 print("AUC is: ", auc)
+
 # Plot ROC curve
 fpr, tpr, thresholds = roc_curve(labels, data['mean_significance'])
 plt.plot(fpr, tpr, label=f'ROC curve (AUC = {auc:.2f})')
@@ -29,5 +31,4 @@ plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
 plt.title('ROC Curve')
 plt.legend(loc='lower right')
-plt.savefig('roc_curve.png')
-
+plt.savefig('NC_curve.png')  # Save the plot as an image
